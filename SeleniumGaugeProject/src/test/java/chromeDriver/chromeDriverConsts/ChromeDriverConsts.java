fb98902f986449311ba5324ff205760c43038d44
Emre DENLİ <denli.emree@gmail.com>
2023-04-04 13:40:04 +0300

    update project

--- SeleniumGaugeProject/src/test/java/chromeDriver/chromeDriverConsts/ChromeDriverConsts.java
+++ SeleniumGaugeProject/src/test/java/chromeDriver/chromeDriverConsts/ChromeDriverConsts.java
@@ -4,11 +4,11 @@ public class ChromeDriverConsts {
 
     public static String DRIVER_JSON = "src/test/java/chromeDriver/chromeDriverJson/chromeDriverVersion.json";
     public static String DRIVER_DOWNLOAD_PATH = "C:\\Users\\testinium\\Downloads\\chromedriver_win32.zip";
-    public static String MAIN_PATH = "C:\\Users\\testinium\\Desktop\\ObiletSeleniumGaugeProjectGit\\SeleniumGaugeProject\\";
-    public static String DRIVER_DESTDIR_PATH = MAIN_PATH + "src\\test\\resources\\driver";
+    public static String MAIN_PATH = "C:\\Users\\testinium\\Desktop\\";
+    public static String DRIVER_DESTDIR_PATH = MAIN_PATH + "ObiletSeleniumGaugeProjectGit\\SeleniumGaugeProject\\src\\test\\resources\\driver";
 
     //public static String PROJECT_DESTDIR_PATH = "src/test/resources/driver";
-    public static String DRIVER_JSON_LONG_PATH = MAIN_PATH + "src\\test\\java\\chromeDriver\\chromeDriverJson\\chromeDriverVersion.json";
-    public static String DRIVER_LICENSE_PATH = MAIN_PATH + "src\\test\\resources\\driver\\LICENSE.chromedriver";
+    public static String DRIVER_JSON_LONG_PATH = MAIN_PATH + "ObiletSeleniumGaugeProjectGit\\SeleniumGaugeProject\\src\\test\\java\\chromeDriver\\chromeDriverJson\\chromeDriverVersion.json";
+    public static String DRIVER_LICENSE_PATH = MAIN_PATH + "ObiletSeleniumGaugeProjectGit\\SeleniumGaugeProject\\src\\test\\resources\\driver\\LICENSE.chromedriver";
 
 }
